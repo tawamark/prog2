2a9cd064a2605117d8a3ec5021d92d523c83fa7b
tawamark <thauangustavok@gmail.com>
2025-08-18 10:28:31 -0300

    veiculo-2

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-// Inclua o arquivo do model Veiculo
 require_once 'models/Veiculo.php';
 
-// Crie uma instância do modelo Veiculo
 $veiculo = new Veiculo('Ford', '1', 'Fusion', 2020);
 
-// Exiba os detalhes do veículo
 echo "Detalhes do Veículo" . PHP_EOL;
 echo "Marca: " . $veiculo->getMarca() . PHP_EOL;
 echo "Modelo: " . $veiculo->getModelo() . PHP_EOL;
